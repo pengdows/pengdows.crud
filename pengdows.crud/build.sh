b4ae7d8757f315516c08b2eef5f116011a36a30d
Alaric Dailey <alaricd@pengdows.com>
2025-06-23 18:02:50 -0500

    Update build.sh

--- pengdows.crud/build.sh
+++ pengdows.crud/build.sh
@@ -16,5 +16,5 @@ sed -i "s|<Version>.*</Version>|<Version>$VERSION</Version>|" pengdows.crud.cspr
 
 # Build and pack
 dotnet pack -c Release
-dotnet nuget push ./bin/Debug/pengdows.crud.1.0.${EPOCH}.nupkg --api-key $(cat ~/token.txt) --source https://api.nuget.org/v3/index.json
+dotnet nuget push ./bin/Release/pengdows.crud.1.0.${EPOCH}.nupkg --api-key $(cat ~/token.txt) --source https://api.nuget.org/v3/index.json
 
