f81e4b96ce5d8a3d8143f58349e1fb187394b15c
Alaric Dailey <alaricd@pengdows.com>
2025-07-03 08:46:13 -0500

    Publish abstractions before dependents

--- build.sh
+++ build.sh
@@ -16,12 +16,12 @@ update_version() {
   sed -i "s|<Version>.*</Version>|<Version>${VERSION}</Version>|" "$csproj"
 }
 
-update_version pengdows.crud/pengdows.crud.csproj
 update_version pengdows.crud.abstractions/pengdows.crud.abstractions.csproj
 update_version pengdows.crud.fakeDb/pengdows.crud.fakeDb.csproj
+update_version pengdows.crud/pengdows.crud.csproj
 
 # Build and pack each project
-for proj in pengdows.crud pengdows.crud.abstractions pengdows.crud.fakeDb; do
+for proj in pengdows.crud.abstractions pengdows.crud.fakeDb pengdows.crud; do
   dotnet pack "$proj/$proj.csproj" -c Release
   pkg="${proj}/bin/Release/${proj}.${VERSION_SHORT}.nupkg"
   dotnet nuget push "$pkg" --source https://api.nuget.org/v3/index.json --api-key "$API_KEY"
