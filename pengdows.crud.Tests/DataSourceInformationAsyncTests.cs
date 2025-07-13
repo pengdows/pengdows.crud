44289c9578fe190d31dbae05c8c108bcb6b7f812
Alaric Dailey <alaricd@pengdows.com>
2025-07-13 13:21:37 -0500

    Refactor async DataSourceInformation test to use FakeDb

--- pengdows.crud.Tests/DataSourceInformationAsyncTests.cs
+++ pengdows.crud.Tests/DataSourceInformationAsyncTests.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
+using System.Data;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging.Abstractions;
-using Moq;
 using pengdows.crud;
 using pengdows.crud.FakeDb;
 using pengdows.crud.wrappers;
@@ -14,6 +14,47 @@ namespace pengdows.crud.Tests;
 
 public class DataSourceInformationAsyncTests
 {
+    private sealed class ThrowingCommand : DbCommand
+    {
+        private readonly DbConnection _connection;
+
+        public ThrowingCommand(DbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public override string? CommandText { get; set; }
+        public override int CommandTimeout { get; set; }
+        public override CommandType CommandType { get; set; }
+        public override bool DesignTimeVisible { get; set; }
+        public override UpdateRowSource UpdatedRowSource { get; set; }
+
+        protected override DbConnection DbConnection
+        {
+            get => _connection;
+            set { }
+        }
+
+        protected override DbParameterCollection DbParameterCollection { get; } = new FakeParameterCollection();
+        protected override DbTransaction? DbTransaction { get; set; }
+
+        public override void Cancel() { }
+        public override int ExecuteNonQuery() => throw new InvalidOperationException();
+        public override object? ExecuteScalar() => throw new InvalidOperationException();
+        public override void Prepare() { }
+
+        protected override DbDataReader ExecuteDbDataReader(CommandBehavior behavior) => throw new InvalidOperationException();
+
+        protected override Task<DbDataReader> ExecuteDbDataReaderAsync(CommandBehavior behavior, CancellationToken cancellationToken)
+            => throw new InvalidOperationException();
+
+        protected override DbParameter CreateDbParameter() => new FakeDbParameter();
+    }
+
+    private sealed class ThrowingConnection : FakeDbConnection
+    {
+        protected override DbCommand CreateDbCommand() => new ThrowingCommand(this);
+    }
     private static async Task<bool> InvokeIsSqliteAsync(ITrackedConnection conn)
     {
         var method = typeof(DataSourceInformation).GetMethod("IsSqliteAsync", BindingFlags.NonPublic | BindingFlags.Static)!;
@@ -44,15 +85,13 @@ public class DataSourceInformationAsyncTests
     [Fact]
     public async Task IsSqliteAsync_ReturnsFalse_WhenCommandFails()
     {
-        var command = new Mock<DbCommand>();
-        command.Setup(c => c.ExecuteReaderAsync(It.IsAny<CommandBehavior>(), It.IsAny<CancellationToken>()))
-            .ThrowsAsync(new InvalidOperationException());
-        command.Setup(c => c.DisposeAsync()).Returns(ValueTask.CompletedTask);
+        var conn = new ThrowingConnection
+        {
+            ConnectionString = $"Data Source=test;EmulatedProduct={SupportedDatabase.Sqlite}"
+        };
+        using var tracked = new TrackedConnection(conn);
 
-        var conn = new Mock<ITrackedConnection>();
-        conn.Setup(c => c.CreateCommand()).Returns(command.Object);
-
-        var result = await InvokeIsSqliteAsync(conn.Object);
+        var result = await InvokeIsSqliteAsync(tracked);
 
         Assert.False(result);
     }
