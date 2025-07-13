7ac5ab0288ebfcb07999afadb1194dac689391cc
Dailey, Alaric <alaricd@pengdows.com>
2025-07-13 06:30:44 -0500

    update testbed

--- testbed/TestProvider.cs
+++ testbed/TestProvider.cs
@@ -14,7 +14,7 @@ public class TestProvider : IAsyncTestProvider
     public TestProvider(IDatabaseContext databaseContext, IServiceProvider serviceProvider)
     {
         _context = databaseContext; //serviceProvider.GetService<IAuditValueResolver>()
-        _helper = new EntityHelper<TestTable, long>(databaseContext);
+        _helper = new EntityHelper<TestTable, long>(databaseContext, auditValueResolver: new StubAuditValueResolver("system"));
     }
 
 
