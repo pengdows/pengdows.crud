90d98a7ae1b69595a9389cd46aba011d3b9e88ae
Alaric Dailey <alaricd@pengdows.com>
2025-07-15 05:59:31 -0500

    Add basic tests for AuditValueResolver

---
+++ pengdows.crud.Tests/AuditValueResolverTests.cs
@@ -0,0 +1,37 @@
+#region
+
+using System;
+using Xunit;
+
+#endregion
+
+namespace pengdows.crud.Tests;
+
+public class AuditValueResolverTests
+{
+    private class TestResolver : AuditValueResolver
+    {
+        private readonly IAuditValues _values;
+        public TestResolver(IAuditValues values) => _values = values;
+        public override IAuditValues Resolve() => _values;
+    }
+
+    [Fact]
+    public void AuditValueResolver_IsAbstract()
+    {
+        Assert.True(typeof(AuditValueResolver).IsAbstract);
+        Assert.True(typeof(IAuditValueResolver).IsAssignableFrom(typeof(AuditValueResolver)));
+    }
+
+    [Fact]
+    public void Resolve_ReturnsExpectedValues()
+    {
+        var expected = new AuditValues { UserId = "u" };
+        var resolver = new TestResolver(expected);
+
+        var result = resolver.Resolve();
+
+        Assert.Same(expected, result);
+    }
+}
+
