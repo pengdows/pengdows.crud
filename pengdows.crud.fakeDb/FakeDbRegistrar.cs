83ecf4b6fdf5698bf76b76b8b03ea74744d11e5d
Alaric Dailey <alaricd@pengdows.com>
2025-08-08 12:44:52 -0500

    fix: restore pengdows namespace

--- pengdows.crud.fakeDb/FakeDbRegistrar.cs
+++ pengdows.crud.fakeDb/FakeDbRegistrar.cs
@@ -1,10 +1,8 @@
-#region
-
+using System;
+using System.Collections.Generic;
 using System.Data.Common;
 
-#endregion
-
-namespace pengdows.crud.FakeDb.pengdows.crud.enums;
+namespace pengdows.crud.FakeDb;
 
 public class FakeDbRegistrar
 {
@@ -26,6 +24,8 @@ public class FakeDbRegistrar
     public void RegisterAll(Dictionary<string, string> providerFactories)
     {
         foreach (var kvp in providerFactories)
+        {
             DbProviderFactories.RegisterFactory(kvp.Key, new FakeDbFactory(kvp.Value));
+        }
     }
 }
