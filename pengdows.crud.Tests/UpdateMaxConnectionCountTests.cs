1c34021c4b5570ed5ef7430ab69d4f977733d4a7
Alaric Dailey <alaricd@pengdows.com>
2025-08-24 12:16:48 -0500

    Clarify variable names in UpdateMaxConnectionCount

---
+++ pengdows.crud.Tests/UpdateMaxConnectionCountTests.cs
@@ -0,0 +1,51 @@
+using System.Reflection;
+using pengdows.crud.configuration;
+using pengdows.crud.enums;
+using pengdows.crud.FakeDb;
+using Xunit;
+
+namespace pengdows.crud.Tests;
+
+public class UpdateMaxConnectionCountTests
+{
+    private static MethodInfo GetMethod()
+        => typeof(DatabaseContext).GetMethod("UpdateMaxConnectionCount", BindingFlags.Instance | BindingFlags.NonPublic)!;
+
+    private static FieldInfo GetField()
+        => typeof(DatabaseContext).GetField("_maxNumberOfOpenConnections", BindingFlags.Instance | BindingFlags.NonPublic)!;
+
+    private static DatabaseContext CreateContext()
+    {
+        var config = new DatabaseContextConfiguration
+        {
+            ConnectionString = "Data Source=:memory:",
+            ProviderName = SupportedDatabase.Sqlite.ToString(),
+            DbMode = DbMode.SingleConnection
+        };
+
+        var factory = new FakeDbFactory(SupportedDatabase.Sqlite);
+        return new DatabaseContext(config, factory);
+    }
+
+    [Fact]
+    public void UpdateMaxConnectionCount_IncreasesWhenHigher()
+    {
+        using var ctx = CreateContext();
+        GetField().SetValue(ctx, 5L);
+
+        GetMethod().Invoke(ctx, new object[] { 10L });
+
+        Assert.Equal(10L, (long)GetField().GetValue(ctx)!);
+    }
+
+    [Fact]
+    public void UpdateMaxConnectionCount_DoesNotDecrease()
+    {
+        using var ctx = CreateContext();
+        GetField().SetValue(ctx, 10L);
+
+        GetMethod().Invoke(ctx, new object[] { 5L });
+
+        Assert.Equal(10L, (long)GetField().GetValue(ctx)!);
+    }
+}
