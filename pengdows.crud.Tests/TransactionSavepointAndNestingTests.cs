7392485583bd9217aea7fff399e3ebe014ce9af1
Dailey, Alaric <alaricd@pengdows.com>
2025-09-01 05:08:23 -0500

    test: cover parameter name normalization and transaction savepoints/nesting

---
+++ pengdows.crud.Tests/TransactionSavepointAndNestingTests.cs
@@ -0,0 +1,37 @@
+#region
+
+using System;
+using System.Data;
+using System.Threading.Tasks;
+using pengdows.crud.enums;
+using Xunit;
+
+#endregion
+
+namespace pengdows.crud.Tests;
+
+public class TransactionSavepointAndNestingTests : SqlLiteContextTestBase
+{
+    [Fact]
+    public async Task Savepoint_RollbackToSavepoint_Completes()
+    {
+        await using var tx = Context.BeginTransaction();
+
+        // Should not throw even if provider treats them as no-ops
+        await tx.SavepointAsync("sp1");
+        await tx.RollbackToSavepointAsync("sp1");
+
+        tx.Commit();
+        Assert.True(tx.WasCommitted);
+        Assert.False(tx.WasRolledBack);
+    }
+
+    [Fact]
+    public void NestedTransaction_Throws()
+    {
+        using var tx = Context.BeginTransaction();
+        var asCtx = (IDatabaseContext)tx;
+        Assert.Throws<InvalidOperationException>(() =>
+            asCtx.BeginTransaction(IsolationLevel.ReadCommitted, ExecutionType.Write));
+    }
+}
