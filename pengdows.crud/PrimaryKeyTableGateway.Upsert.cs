023e9fd2dd80d02fa22db6f464ffb9ea4ae92e45
Dailey, Alaric <alaricd@pengdows.com>
2026-08-19 11:52:06 -0500

    fix: omit terminator from Oracle merge
    (cherry picked from commit fa508c92f3bc16c6e3ec67c36ac51b9a0e54d761)

--- pengdows.crud/PrimaryKeyTableGateway.Upsert.cs
+++ pengdows.crud/PrimaryKeyTableGateway.Upsert.cs
@@ -411,7 +411,12 @@ public partial class PrimaryKeyTableGateway<TEntity>
                 .Append(insertColSb.AsSpan())
                 .Append(") VALUES (")
                 .Append(insertValSb.AsSpan())
-                .Append(");");
+                .Append(")");
+
+            if (dialect.DatabaseType != SupportedDatabase.Oracle)
+            {
+                sc.Query.Append(';');
+            }
 
             sc.AddParameters(parameters);
             return sc;
